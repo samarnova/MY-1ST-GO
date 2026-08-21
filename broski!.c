cc18a6c9c505a9ba26bb92ef065e2fb9373df303
samarnova <samardossani29@gmail.com>
2026-08-21 21:32:35 +0500

    Create broski!.c
    a prank webs that asks u what nums u want to add together and when the user goes ahead and typs in those two numbers, it will tell em to do it themselvess
    
    ngl this is a test code

---
+++ broski!.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(void)
+{
+int a, b;
+printf("typ the two numbers u want to add!");
+scanf("%d %d",&a,&b);
+printf("\nbro im not ur asisstant add the twio nums urself! hmph broski!");
+getch();
+}
