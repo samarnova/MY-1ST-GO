ea6cf6e41c61bda8ed0d32bd64414b5726cc769a
samarnova <samardossani29@gmail.com>
2026-08-25 01:55:52 +0500

    Create initial C project with console output
    Added a C program that prints a message about the project experience.

---
+++ SAMAR'SINDACLUBNOWYALLGOINDOWN.C.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int  main (void)
+{
+printf("\033[38;2;180;160;230m haiii!!howaauuu??");
+printf( "\nTHIS IS MY FIRST EVER INDEPENDENT PROJECT IN C LANGUAGE");
+printf("\nDAY 1 OF MAKING A PROJECT IN C LANGUAGE");
+printf("\nSo im using vs code rn .... but im having issues no cap.It wont compile itself like as in itself i mean vs doesnt have an ooption  or such function.");
+printf("\nbruh whats the even point of having installed thiss if it wont compile itself??");
+printf("\nbut ngl im having a thrill cz vs is matchin my energy!");
+printf("\nbut while coding and typing only !!");
+printf("\nig that swhy im typing as much..haha");
+printf("\ncz whtevrz after this is a hassle!");
+printf("\n\033[38;2;160;210;240m AHH SOW CUTEW!MUAHH ! BYEII!");
+getchar();
+}
